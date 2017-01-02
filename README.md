be5cfaf092eaa60a6a7a2ecedf517687c40c25e4
EC2 Default User <ec2-user@ip-172-31-57-241.ec2.internal>
2017-01-02 01:48:52 +0000

    Install basics via curl

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# salt-git-repos
-Salt formula to clone and update a set of git repositories
+=================================================
+Salt for cloning Git Repositories
+=================================================
+
+Salt formula to clone and update a set of git repositories.
+
+Add public key to the repositories you will need, this includes both the main Django project and any applications you may need. This is how to easily create your private and public keys locally without a prompt or passphrase:
+
+  ``echo -e 'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''``
+
+Your public key, which you should add to Github, should be in:
+
+  ``cat ~/.ssh/id_rsa.pub`` 
+
+A quick way to add a publick key to a host with Ubuntu is:
+
+  ``ssh-copy-id user@host`` 
+
+Optionally, if you want avoid the prompt when cloning this repository from Github (which happens when running the quick install script), you can add the fingerprint like this:
+
+  ``ssh-keyscan github.com >> ~/.ssh/known_hosts``
+
+  ``\curl -sSL https://raw.githubusercontent.com/alexisbellido/salt-git-repos/master/scripts/install-prerequisites-amazon-linux.sh | sudo bash -s full|master|minion "Joe Doe" name@example.com``
+
+You need three arguments:
+
+The first one defines the type of installation: "full" to install both salt-master and salt-minion, "master" to install only salt-master, or "minion" to install only salt-minion.
+The second and third arguments are used to setup git --global user.name and user.email.
+
+An installation of type full or master will also copy basic top.sls to /srv/salt/top.sls and /srv/pillar/* and files and point to them from /etc/salt/master.
---
+++ scripts/install-prerequisites-amazon-linux.sh
@@ -0,0 +1,92 @@
+#!/bin/bash -e
+
+# Install git and Salt with basic configuration on Amazon Linux
+
+if [ -z "$1" ]; then
+
+  echo
+  echo "Usage (run from the root of this repository):"
+  echo "sudo scripts/install-prerequisites-amazon-linux.sh full git_user_name git_user_email"
+  echo 
+  echo "Example:"
+  echo "sudo scripts/install-prerequisites-amazon-linux.sh full \"Joe Doe\" name@example.com"
+  echo "Use quotes if the name contains spaces."
+  echo
+  echo "Run it remotely:"
+  echo "\curl -sSL https://raw.githubusercontent.com/alexisbellido/salt-git-repos/master/scripts/install-prerequisites-amazon-linux.sh | sudo bash -s full \"Joe Doe\" name@example.com"
+  echo
+
+else
+  TOP_DIR="/srv/salt"
+  PILLAR_DIR="/srv/pillar"
+
+  if [ "$1" == "minion" -o "$1" == "master" -o "$1" == "full" ]; then
+
+    echo
+    echo "Installing prerequisites..."
+    echo
+
+    yum update -y
+    yum install git -y
+    yum install vim -y
+
+  fi
+  
+  if [ "$1" == "master" -o "$1" == "full" ]; then
+
+#    apt-get install -y salt-master
+
+    if [[ $SUDO_COMMAND == "/bin/bash -s"* ]]; then
+      ROOT_DIR="$PWD/salt-git-repos"
+    else
+      ROOT_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
+    fi
+
+    if [ ! -d  "$ROOT_DIR" ]; then
+      sudo -u $SUDO_USER git clone git@github.com:alexisbellido/salt-git-repos.git
+    fi
+    
+    if [ ! -d  "$TOP_DIR" ]; then
+      echo "Creating $TOP_DIR..."
+      mkdir -p $TOP_DIR
+      cp $ROOT_DIR/conf/srv/salt/top.sls $TOP_DIR
+    fi
+    
+    if [ ! -d  "$PILLAR_DIR" ]; then
+      echo "Creating $PILLAR_DIR..."
+      mkdir -p $PILLAR_DIR
+      cp -r $ROOT_DIR/conf/srv/pillar/* $PILLAR_DIR
+    fi
+
+#    sed -i '/^# Added by install script$/,$d' /etc/salt/master
+#    cat >> /etc/salt/master << EOL
+#
+## Added by install script
+#file_roots:
+#  base:
+#    - /srv/salt
+#    - ${ROOT_DIR}
+#
+#pillar_roots:
+#  base:
+#    - ${PILLAR_DIR}
+#  staging:
+#    - ${PILLAR_DIR}/staging
+#  production:
+#    - ${PILLAR_DIR}/production
+#EOL
+#
+#    service salt-master restart
+
+  fi
+  
+#  if [ "$1" == "minion" -o "$1" == "full" ]; then
+#    apt-get install -y salt-minion
+#  fi
+#  
+#  if [ "$1" == "minion" -o "$1" == "master" -o "$1" == "full" ]; then
+#    git config --global user.name "$2"
+#    git config --global user.email $3
+#  fi
+
+fi
